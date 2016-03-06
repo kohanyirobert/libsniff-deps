584daedb210bdd9cb52112fdd5ed486a37dba5a5
Robert Kohanyi <robert.kohanyi@metapack.com>
2016-03-06 16:59:32 +0000

    Update to use libsniff-download

--- index.js
+++ index.js
@@ -1,93 +1,25 @@
-var Decompress = require('decompress')
-var mkdirp = require('mkdirp')
-var path = require('path')
-var vargs = require('vargs-callback')
-var rimraf = require('rimraf')
-var request = require('request')
-var fs = require('fs')
-var package = require('libsniff-ffmpeg-binaries/package.json')
+var download = require('libsniff-download')
+var _ = require('lodash')
 
-var baseUrl = 'https://github.com/kohanyirobert/'
-baseUrl += package.name
-baseUrl += '/releases/download/v'
-baseUrl += package.version
-baseUrl += '/'
+var pkg = require('libsniff-ffmpeg-binaries/package.json')
 
 var getUrl = function(target) {
-  return baseUrl + getZip(target)
+  var url = 'https://github.com/kohanyirobert/'
+  url += pkg.name
+  url += '/releases/download/v'
+  url += pkg.version
+  url += '/'
+  return url + getDestName(target)
 }
 
-var getPath = function(opts, target) {
-  return path.join(opts.dir, getZip(target))
-}
-
-var getZip = function(target) {
+var getDestName = function(target) {
   return target + '.zip'
 }
 
-var doDecompress = function(opts, target, cb) {
-  new Decompress()
-    .src(getPath(opts, target))
-    .dest(opts.dir)
-    .use(Decompress.zip())
-    .run(function(err, files) {
-      if (err) {
-        throw new Error(err)
-      }
-      fs.unlink(getPath(opts, target))
-      cb(files)
-    })
+module.exports = function(opts, cb) {
+  download(_.defaults(opts, {
+    archive: true,
+    getUrl: getUrl,
+    getDestName: getDestName
+  }), cb)
 }
-
-var doCollect = function(opts, target, downloads, files, cb) {
-  downloads.push({
-    target: target,
-    files: files.map(function(file) {
-      return file.path
-    })
-  })
-  if (downloads.length === opts.targets.length) {
-    cb(downloads)
-  }
-}
-
-var doTargets = function(opts, cb) {
-  var downloads = []
-  opts.targets.forEach(function(target) {
-    var out = fs.createWriteStream(getPath(opts, target))
-    out.on('finish', function() {
-      doDecompress(opts, target, function(files) {
-        doCollect(opts, target, downloads, files, cb)
-      })
-    })
-    request(getUrl(target)).pipe(out)
-  })
-}
-
-var doCreate = function(opts, cb) {
-  mkdirp(opts.dir, function() {
-    doTargets(opts, cb)
-  })
-}
-
-var doCleanup = function(opts, cb) {
-  rimraf(opts.dir, function() {
-    doCreate(opts, cb)
-  })
-}
-
-var doDownload = function(opts, cb) {
-  opts = opts || {}
-  opts.dir = opts.dir || 'ffmpeg'
-  opts.targets = opts.targets || [
-    'linux-ia32',
-    'linux-x64',
-    'win32-ia32',
-    'win32-x64',
-    'darwin-x64'
-  ]
-  cb = cb || function() {}
-  doCleanup(opts, cb)
-}
-
-module.exports = vargs.strict(doDownload)
--- test.js
+++ test.js
@@ -1,17 +1,17 @@
 var assert = require('chai').assert
 var tmp = require('tmp')
 var download = require('./')
-var rimraf = require('rimraf')
-var package = require('./package.json')
+var path = require('path')
+var pkg = require('./package.json')
 
 var THIRTY_SECONDS = 30 * 1000
 
-describe(package.name, function() {
+describe(pkg.name, function() {
   it('should download the specfied targets', function(done) {
     this.timeout(THIRTY_SECONDS)
-    tmp.dir({unsafeCleanup: true}, function(err, path) {
+    tmp.dir({unsafeCleanup: true}, function(err, tmpDir) {
       var options = {
-        dir: path,
+        dir: tmpDir,
         targets: [
           'linux-ia32',
           'darwin-x64'
@@ -24,9 +24,9 @@ describe(package.name, function() {
           assert(options.targets.indexOf(download.target) > -1)
           assert(download.files.length === 2)
           download.files.forEach(function(file) {
-            assert(file.indexOf(options.dir) === 0)
-            assert(file.indexOf('ffmpeg') > -1
-                || file.indexOf('ffprobe') > -1)
+            var destPath = path.resolve(path.join(options.dir, download.target))
+            assert(file.indexOf(destPath) === 0)
+            assert(file.indexOf('ffmpeg') > -1 || file.indexOf('ffprobe') > -1)
           })
         })
         done()
